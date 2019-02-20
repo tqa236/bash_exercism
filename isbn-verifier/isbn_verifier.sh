10fa5f9fa5815f7b98095dd359f5819c2da4d7f0
TQA <trinhquocanh94@gmail.com>
2019-02-21 00:30:44 +0100

    Solve ISBN verifier

--- isbn-verifier/isbn_verifier.sh
+++ isbn-verifier/isbn_verifier.sh
@@ -3,7 +3,24 @@ set -o errexit
 set -o nounset
 
 main() {
-    date -u -d "$1 + 1000000000 seconds" "+%a %b %-d %T %Z %Y"
+    digits="${1//[^0-9]/}"
+    ([ ${#digits} != 9 ] && [ ${#digits} != 10 ]) && echo "false" && exit 0
+
+    check=0
+    if [ ${#digits} == 9 ]; then
+        if [ "${1: -1:1}" == "X" ]; then
+            check=$((check + 10))
+        else
+            echo "false" && exit 0
+        fi
+    fi
+
+    for ((i=0;i<${#digits};i++))
+    do
+        check=$((check + ${digits:$i:1} * (10 - i)))
+    done
+    (( check%11 == 0 )) && echo "true" && exit 0
+    echo "false"
 }
 
 main "$@"
