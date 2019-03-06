2539d0b842c0037e8688896bb4e8bbd04fa52878
TQA <trinhquocanh94@gmail.com>
2019-03-06 01:41:15 +0100

    Solve scrabble score

--- scrabble-score/scrabble_score.sh
+++ scrabble-score/scrabble_score.sh
@@ -2,8 +2,23 @@
 set -o errexit
 set -o nounset
 
+ord() {
+  LC_CTYPE=C printf '%d' "'$1"
+}
+
 main() {
-    echo 0
+    input=${1,,}
+    values=(1 3 3 2 1 4 2 4 1 8 5 1 3 1 1 3 10 1 1 1 1 4 4 8 4 10)
+    score=0
+
+    for ((i=0;i<${#input};i++))
+    do
+        value="$(ord "${input:$i:1}")"
+        value=$((value - 97))
+        score=$((score + values[value]))
+    done
+
+    echo "$score"
 }
 
 main "$@"
--- scrabble-score/scrabble_score_test.sh
+++ scrabble-score/scrabble_score_test.sh
@@ -1,89 +1,90 @@
 #!/usr/bin/env bash
 
 @test 'lowercase letter' {
-  #skip
+  # skip
   run bash scrabble_score.sh 'a'
-  
+  echo "status = ${status}"
+  echo "output = ${output}"
   [ "$status" -eq 0 ]
   [ "$output" -eq 1 ]
 }
 
 @test 'uppercase letter' {
-  skip
+  # skip
   run bash scrabble_score.sh 'A'
-  
+
   [ "$status" -eq 0 ]
   [ "$output" -eq 1 ]
 }
 
 @test 'valuable letter' {
-  skip
+  # skip
   run bash scrabble_score.sh 'f'
-  
+
   [ "$status" -eq 0 ]
   [ "$output" -eq 4 ]
 }
 
 @test 'short word' {
-  skip
+  # skip
   run bash scrabble_score.sh 'at'
-  
+
   [ "$status" -eq 0 ]
   [ "$output" -eq 2 ]
 }
 
 @test 'short, valuable word' {
-  skip
+  # skip
   run bash scrabble_score.sh 'zoo'
-  
+
   [ "$status" -eq 0 ]
   [ "$output" -eq 12 ]
 }
 
 @test 'medium word' {
-  skip
+  # skip
   run bash scrabble_score.sh 'street'
-  
+
   [ "$status" -eq 0 ]
   [ "$output" -eq 6 ]
 }
 
 @test 'medium, valuable word' {
-  skip
+  # skip
   run bash scrabble_score.sh 'quirky'
-  
+
   [ "$status" -eq 0 ]
   [ "$output" -eq 22 ]
 }
 
 @test 'long, mixed-case word' {
-  skip
+  # skip
   run bash scrabble_score.sh 'OxyphenButazone'
-  
+
   [ "$status" -eq 0 ]
   [ "$output" -eq 41 ]
 }
 
 @test 'english-like word' {
-  skip
+  # skip
   run bash scrabble_score.sh 'pinata'
-  
+
   [ "$status" -eq 0 ]
   [ "$output" -eq 8 ]
 }
 
 @test 'empty input' {
-  skip
+  # skip
   run bash scrabble_score.sh ''
-  
+
   [ "$status" -eq 0 ]
   [ "$output" -eq 0 ]
 }
 
 @test 'entire alphabet available' {
-  skip
+  # skip
   run bash scrabble_score.sh 'abcdefghijklmnopqrstuvwxyz'
-  
+
   [ "$status" -eq 0 ]
   [ "$output" -eq 87 ]
 }
