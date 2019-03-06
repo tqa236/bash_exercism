22b1908dd30e9f409637ca5c543fa707d55617fd
TQA <trinhquocanh94@gmail.com>
2019-03-06 23:31:02 +0100

    Pure bash solution for pangram

--- pangram/pangram.sh
+++ pangram/pangram.sh
@@ -3,10 +3,21 @@ set -o errexit
 set -o nounset
 
 main() {
-    count="$(echo "${1//[^a-zA-Z]/}" | grep -o . | sort | uniq -i | wc -l)"
-    if (( count == 26 )); then echo "true"
-    else echo "false"
-    fi
+    input=${1,,}
+
+    for char in {a..z}
+    do
+        if [[ $input != *$char* ]]; then
+            echo "false"
+            exit 0
+        fi
+    done
+    echo "true"
+    
+    # count="$(echo "${1//[^a-zA-Z]/}" | grep -o . | sort | uniq -i | wc -l)"
+    # if (( count == 26 )); then echo "true"
+    # else echo "false"
+    # fi
 }
 
 main "$@"
