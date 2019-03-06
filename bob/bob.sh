88d826a8ac67cf9754e85adcabe582103693fad6
TQA <trinhquocanh94@gmail.com>
2019-03-06 01:15:02 +0100

    Improve bob

--- bob/bob.sh
+++ bob/bob.sh
@@ -2,27 +2,33 @@
 set -o errexit
 set -o nounset
 
+is_silence(){
+    [ -z "$input" ]
+}
+
+is_yelling(){
+    [ "${input}" == "${input^^}" ] && [ "${input^^}" != "${input,,}" ]
+}
+
+is_question(){
+    [ "${input: -1}" == "?" ]
+}
+
 main() {
     input="$(echo "${@//\'/}" | xargs)"
 
-    if [ -z "$input" ]; then
-        echo "Fine. Be that way!" && exit 0
+    if is_silence; then
+        echo "Fine. Be that way!"
+    elif is_yelling && is_question; then
+        echo "Calm down, I know what I'm doing!"
+    elif is_yelling; then
+        echo "Whoa, chill out!"
+    elif is_question; then
+        echo "Sure."
+    else
+        echo "Whatever."
     fi
 
-    if ([ "${input}" == "${input^^}" ] && [ "${input^^}" != "${input,,}" ]);
-        then
-            if [ "${input: -1}" == "?" ];
-                then echo "Calm down, I know what I'm doing!"
-            else
-                echo "Whoa, chill out!"
-            fi
-    else
-        if [ "${input: -1}" == "?" ];
-            then echo "Sure."
-        else
-            echo "Whatever."
-        fi
-    fi
 }
 
 main "$@"
