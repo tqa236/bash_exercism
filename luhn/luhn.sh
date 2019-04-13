a1794230e16824751fd770521c21fbf32d7def2a
TQA <trinhquocanh94@gmail.com>
2019-04-13 12:38:17 +0200

    Improve Luhn solution

--- luhn/luhn.sh
+++ luhn/luhn.sh
@@ -5,12 +5,11 @@ set -o nounset
 main() {
     reverse_input="$(echo "${1// /}"|rev)"
 
-    [[ "$reverse_input" == "0" ]] && echo "false" && exit 0
-    [[ "${reverse_input//[0-9]/}" != "" ]] && echo "false" && exit 0
+    [[ $reverse_input == 0 ]] && echo "false" && exit 0
+    [[ ${reverse_input//[0-9]/} != "" ]] && echo "false" && exit 0
 
     sum=0
-    for ((i=0;i<${#reverse_input};i++))
-    do
+    for ((i=0;i<${#reverse_input};i++)); do
         digit=${reverse_input:$i:1}
         if ((i % 2 == 0)); then
             sum=$(( sum + digit ))
