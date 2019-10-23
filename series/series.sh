eefc2c6dd85674ec4ea73e15218949b1d19c52b8
TQA <trinhquocanh94@gmail.com>
2019-10-23 17:40:43 +0200

    Update series

--- series/series.sh
+++ series/series.sh
@@ -3,28 +3,21 @@ set -o errexit
 set -o nounset
 
 main() {
-    if [ -z "$1" ]; then
-        echo "series cannot be empty" && exit 1
-    fi
+    [ -z "$1" ] && echo "series cannot be empty" && exit 1
 
     number_length=${#1}
-    if (( $2 >  number_length )); then
+    (( $2 >  number_length )) &&
         echo "slice length cannot be greater than series length" && exit 1
-    fi
-
-    if (( $2 == 0 )); then
-        echo "slice length cannot be zero" && exit 1
-    fi
-
-    if (( $2 < 0 )); then
-        echo "slice length cannot be negative" && exit 1
-    fi
+    (( $2 == 0 )) && echo "slice length cannot be zero" && exit 1
+    (( $2 < 0 )) && echo "slice length cannot be negative" && exit 1
 
     num_series=$(( number_length - $2 + 1 ))
     for ((i=0; i<num_series; i++)); do
-        echo "${1:$i:$2}"
+        printf "%s" "${1:$i:$2}"
+        if (( i < num_series - 1 )); then
+            printf " "
+        fi
     done
-
 }
 
 main "$@"
