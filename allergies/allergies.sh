609c00d3d96343462b4be850e7faf77ee756503c
TQA <trinhquocanh94@gmail.com>
2020-03-21 13:36:26 +0100

    Solve allergies

--- allergies/allergies.sh
+++ allergies/allergies.sh
@@ -2,8 +2,28 @@
 set -o errexit
 set -o nounset
 
+declare -A allergies=(["eggs"]="1" ["peanuts"]="2" ["shellfish"]="4" ["strawberries"]="8" ["tomatoes"]="16" ["chocolate"]="32" ["pollen"]="64" ["cats"]="128")
+allergies_list=("eggs" "peanuts" "shellfish" "strawberries" "tomatoes" "chocolate" "pollen" "cats")
+
 main() {
-    echo "false"
+    if [[ $2 = "allergic_to" ]]; then
+        if (( ($1 & ${allergies[$3]}) == 0 )); then
+            echo "false"
+        else
+            echo "true"
+        fi
+    fi
+
+    if [[ $2 = "list" ]]; then
+        all_allergies=()
+        for allergy in "${allergies_list[@]}"; do
+            if (( ($1 & ${allergies[$allergy]}) != 0 )); then
+                all_allergies+=("$allergy")
+            fi
+        done
+        echo "${all_allergies[@]}"
+    fi
+
 }
 
 main "$@"
