9ae73702a46aa27d7e76e1b7ffe213e2e0ed42d2
TQA <trinhquocanh94@gmail.com>
2020-03-25 22:37:24 +0100

    solve binary search

--- binary-search/binary_search.sh
+++ binary-search/binary_search.sh
@@ -3,7 +3,17 @@ set -o errexit
 set -o nounset
 
 main() {
-    echo "Hello"
+    value=$1
+    IFS=" " read -r -a array <<< "${@:2}"
+    start=0
+    end=${#array[@]}
+    while (( start < end)); do
+        middle=$(( (start + end)/2 ))
+        current_value=${array[$middle]}
+        (( current_value == value )) && echo $middle && exit 0
+        (( current_value < value )) && start=$(( middle + 1 )) || end=$middle
+    done
+    echo -1
 }
 
 main "$@"
