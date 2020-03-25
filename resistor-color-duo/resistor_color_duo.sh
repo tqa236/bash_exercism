be1a3a42b1c8718a3eed8adcaf2e4fdf6a482372
TQA <trinhquocanh94@gmail.com>
2020-03-25 17:05:02 +0100

    Solve registor color duo

--- resistor-color-duo/resistor_color_duo.sh
+++ resistor-color-duo/resistor_color_duo.sh
@@ -2,8 +2,21 @@
 set -o errexit
 set -o nounset
 
+declare -A registor_colors=(["black"]="0" ["brown"]="1" ["red"]="2"
+                            ["orange"]="3" ["yellow"]="4" ["green"]="5"
+                            ["blue"]="6" ["violet"]="7" ["grey"]="8" ["white"]="9")
+
 main() {
-    echo "Hello"
+    value=""
+    (( $# > 2 )) && echo "51" && exit 0
+    for color in "$@"; do
+        if [[ ${registor_colors[$color]+x} ]]; then
+            value="$value${registor_colors[$color]}"
+        else
+            echo "invalid color" && exit 1
+        fi
+    done
+    echo "$value"
 }
 
 main "$@"
