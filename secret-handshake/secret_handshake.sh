d6670fc112f9c961538c60b093204090c1b2155d
Trinh Quoc Anh <trinhquocanh94@gmail.com>
2024-12-15 18:50:32 +0100

    Solve exercise

--- secret-handshake/secret_handshake.sh
+++ secret-handshake/secret_handshake.sh
@@ -1,9 +1,30 @@
-#!/usr/bin/env bash
-set -o errexit
-set -o nounset
+#!/bin/bash
 
-main() {
-    echo "wink"
+reverse_array() {
+    local arr=("$@")
+    local reversed=()
+
+    for ((i=${#arr[@]}-1; i>=0; i--)); do
+        reversed+=("${arr[i]}")
+    done
+
+    IFS=','; echo "${reversed[*]}"
 }
 
-main "$@"
+actions=("wink" "double blink" "close your eyes" "jump")
+
+num=$1
+
+result=()
+
+for i in {0..3}; do
+  if (( num & (1 << i) )); then
+    result+=("${actions[i]}")
+  fi
+done
+
+if (( num & 16 )); then
+  reverse_array "${result[@]}"
+else
+  IFS=','; echo "${result[*]}"
+fi
